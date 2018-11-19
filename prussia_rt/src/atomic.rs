063dac55a063ad00a7cce8d7a2e7c76a16ed354d
Dan Ravensloft <dan.ravensloft@gmail.com>
2018-11-19 21:23:13 +0000

    Make atomic actually atomic

--- prussia_rt/src/atomic.rs
+++ prussia_rt/src/atomic.rs
@@ -1,5 +1,7 @@
 //! Atomic reads and writes.
 
+use crate::interrupts;
+
 extern "C" {
     fn _read_u64(addr: usize) -> u64;
     fn _write_u64(addr: usize, value: u64);
@@ -7,10 +9,14 @@ extern "C" {
 
 /// Read a 64-bit value from an address.
 pub fn read_u64(addr: usize) -> u64 {
-    unsafe { _read_u64(addr) }
+    let mut val = 0;
+    interrupts::free(|| unsafe {
+        val = _read_u64(addr);
+    });
+    val
 }
 
 /// Write a 64-bit value to an address.
 pub fn write_u64(addr: usize, value: u64) {
-    unsafe { _write_u64(addr, value) }
+    interrupts::free(|| unsafe { _write_u64(addr, value) });
 }
