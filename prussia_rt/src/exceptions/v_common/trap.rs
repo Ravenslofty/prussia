8d92df7aa628bcc76899a1e8be2f4f9bb18fd7d3
Zachary Cauchi <zach.cauchi@hotmail.com>
2024-11-02 21:34:42 +0100

    feat(prussia_rt): Improve trap exc trigger and fix missing epc increment

--- prussia_rt/src/exceptions/v_common/trap.rs
+++ prussia_rt/src/exceptions/v_common/trap.rs
@@ -1,15 +1,13 @@
 use prussia_debug::println_ee;
 
-use crate::{cop0::CoP0Dump, thread::ThreadControlBlock};
+use crate::{cop0::CoP0Dump, increment_epc, thread::ThreadControlBlock};
 
 /// Trigger Trap exception.
 pub unsafe fn trigger_trap_exception() {
     println_ee!("Triggering trap.");
 
     core::arch::asm!("
-        li $t1, 0x5
-        li $t2, 0x5
-        teq $t1, $t2
+        teq $zero, $zero
     ");
 
     println_ee!("Returned from exception.");
@@ -21,6 +19,8 @@ pub(super) extern "C" fn v_common_trap_handler(tcb_ptr: *mut ThreadControlBlock)
     let cop0_dump = CoP0Dump::load();
 
     println_ee!("TRAP: Trap instruction evaluated to true.");
+
+    increment_epc!();
 
     println_ee!("TRAP: Returning.");
 }
